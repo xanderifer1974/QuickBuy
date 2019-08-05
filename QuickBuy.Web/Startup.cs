3431bdc42cb27d42a6f0e4bc7f27424fb79c071a
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-05 16:37:25 -0300

    Executando o angular separado do ASP.NET core

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -62,7 +62,8 @@ namespace QuickBuy.Web
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
                 }
             });
         }
