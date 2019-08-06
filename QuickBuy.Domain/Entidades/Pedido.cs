1d43aec1c590e48a44320edb295640f290477227
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-05 23:27:19 -0300

    Definindo as Entidades II

--- QuickBuy.Domain/Entidades/Pedido.cs
+++ QuickBuy.Domain/Entidades/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickBuy.Domain.ObjetoValor;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
@@ -9,6 +10,15 @@ namespace QuickBuy.Domain.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string  Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
         /// <summary>
         /// Pedido deve ter pelo menos um
         /// ou muitos pedidos.
---
+++ QuickBuy.Domain/ObjetoValor/FormaPagamento.cs
@@ -0,0 +1,39 @@
+﻿using QuickBuy.Domain.Enumerados;
+
+namespace QuickBuy.Domain.ObjetoValor
+{
+   public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto {
+
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+
+        }
+
+        public bool EhCartaoCredito
+        {
+
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+
+        }
+
+        public bool EhDeposito  
+        {
+
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+
+        }
+
+        public bool NaoFoiDefimnido
+        {
+
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+
+        }
+
+    }
+}
