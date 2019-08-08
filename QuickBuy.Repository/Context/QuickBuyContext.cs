3c3cf90d74191c4116f2877c5b7e264958281cd5
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-08 08:10:05 -0300

    Configurando o contexto do banco de dados

--- QuickBuy.Repository/Context/QuickBuyContext.cs
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
 using QuickBuy.Domain.Entidades;
 using QuickBuy.Domain.ObjetoValor;
 
 namespace QuickBuy.Repository.Context
 {
-    public class QuickBuyContext: DbContext
-    {
+    public class QuickBuyContext: DbContext    {
+       
+
         public DbSet<Usuario> Usuarios { get; set; }
 
         public DbSet<Produto> Produtos { get; set; }
@@ -15,6 +17,12 @@ namespace QuickBuy.Repository.Context
         public DbSet<ItemPedido> ItemPedidos { get; set; }
 
         public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+        public QuickBuyContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
 
     }
 }
--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -3,24 +3,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Repository.Context;
 
 namespace QuickBuy.Web
 {
     public class Startup
     {
+        /// <summary>
+        /// Local para configurar o acesso ao DbContext
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json", optional:false, reloadOnChange:true); //Neste arquivo será colocada a string de conexão com o banco de dados.
+            Configuration = builder.Build();//Construi a interface de configuração.
         }
 
-        public IConfiguration Configuration { get; }
+       
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Configuração da conexão com o banco de dados 
+            var connectionString = Configuration.GetConnectionString("MySqlConnection");
+            services.AddDbContext<QuickBuyContext>(option => option.UseMySql(connectionString,
+                                                                             m => m.MigrationsAssembly("QuickBuy.Repository")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
