6e38dc5b79a602c84f6fa97c3c78e92ef8ef7b70
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-07 23:23:51 -0300

    Criacao do DbContex e configuracao do DbSet

---
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Domain.Entidades;
+using QuickBuy.Domain.ObjetoValor;
+
+namespace QuickBuy.Repository.Context
+{
+    public class QuickBuyContext: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
